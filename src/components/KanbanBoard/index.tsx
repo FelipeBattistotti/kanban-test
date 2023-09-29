5319a82769bcfdfeda3c643ba689d8aef942f945
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-09-28 23:39:00 -0300

    [UPDATE] 2

--- src/components/KanbanBoard/index.tsx
+++ src/components/KanbanBoard/index.tsx
@@ -12,111 +12,131 @@ type Column = {
   items: Item[];
 };
 
-const initialData: Column[] = [
-  {
-    id: 'column-1',
-    title: 'To Do',
-    items: [
-      { id: 'item-1', content: 'Item 1' },
-      { id: 'item-2', content: 'Item 2' },
-    ],
+const initialData = {
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To Do',
+      itemsIds: ['item-1', 'item-2']
+    },
+    'column-2': {
+      id: 'column-2',
+      title: 'Done',
+      itemsIds: []
+    }
   },
-  {
-    id: 'column-2',
-    title: 'Done',
-    items: [],
+  items: {
+    'item-1': { id: 'item-1', content: 'Item 1' },
+    'item-2': { id: 'item-2', content: 'Item 2' }
   },
-];
+  columnOrder: ['column-1', 'column-2']
+};
 
 const KanbanBoard: React.FC = () => {
-  const [columns, setColumns] = useState(initialData);
+  const [data, setData] = useState(initialData);
 
   const onDragEnd = (result: any) => {
     const { source, destination } = result;
 
-    if (!destination) {
-      return;
-    }
+    if (!destination) return;
 
-    if (source.droppableId === destination.droppableId && source.index === destination.index) {
-      return;
-    }
+    if (source.droppableId === destination.droppableId && source.index === destination.index) return;
 
-    const startColumn = columns.find(col => col.id === source.droppableId)!;
-    const endColumn = columns.find(col => col.id === destination.droppableId)!;
+    const startColumn = data.columns[source.droppableId];
+    const endColumn = data.columns[destination.droppableId];
 
     if (startColumn === endColumn) {
-      const newItems = Array.from(startColumn.items);
-      const [removed] = newItems.splice(source.index, 1);
-      newItems.splice(destination.index, 0, removed);
+      const newItemsIds = Array.from(startColumn.itemsIds);
+      newItemsIds.splice(source.index, 1);
+      newItemsIds.splice(destination.index, 0, result.draggableId);
 
       const newColumn = {
         ...startColumn,
-        items: newItems,
+        itemsIds: newItemsIds
       };
 
-      setColumns(prev => prev.map(col => (col.id === newColumn.id ? newColumn : col)));
+      setData(prevData => ({
+        ...prevData,
+        columns: {
+          ...prevData.columns,
+          [newColumn.id]: newColumn
+        }
+      }));
     } else {
-      const startItems = Array.from(startColumn.items);
-      const [removed] = startItems.splice(source.index, 1);
+      const startItemsIds = Array.from(startColumn.itemsIds);
+      startItemsIds.splice(source.index, 1);
+      const newStartColumn = {
+        ...startColumn,
+        itemsIds: startItemsIds
+      };
 
-      const endItems = Array.from(endColumn.items);
-      endItems.splice(destination.index, 0, removed);
+      const endItemsIds = Array.from(endColumn.itemsIds);
+      endItemsIds.splice(destination.index, 0, result.draggableId);
+      const newEndColumn = {
+        ...endColumn,
+        itemsIds: endItemsIds
+      };
 
-      setColumns(prev => 
-        prev.map(col => 
-          col.id === startColumn.id ? { ...startColumn, items: startItems } :
-          col.id === endColumn.id ? { ...endColumn, items: endItems } : 
-          col
-        )
-      );
+      setData(prevData => ({
+        ...prevData,
+        columns: {
+          ...prevData.columns,
+          [newStartColumn.id]: newStartColumn,
+          [newEndColumn.id]: newEndColumn
+        }
+      }));
     }
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div style={{ display: 'flex', gap: '16px' }}>
-        {columns.map(column => (
-          <Droppable key={column.id} droppableId={column.id}>
-            {(provided, snapshot) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                style={{
-                  width: 240,
-                  backgroundColor: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
-                  padding: 8,
-                  borderRadius: 4,
-                }}
-              >
-                <h3>{column.title}</h3>
-                {column.items.map((item, index) => (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={{
-                          userSelect: 'none',
-                          padding: 8,
-                          margin: '0 0 8px 0',
-                          borderRadius: 4,
-                          backgroundColor: snapshot.isDragging ? 'lightgreen' : 'grey',
-                          color: 'white',
-                          ...provided.draggableProps.style,
-                        }}
-                      >
-                        {item.content}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        ))}
+        {data.columnOrder.map(columnId => {
+          const column = data.columns[columnId];
+          const items = column.itemsIds.map(itemId => data.items[itemId]);
+
+          return (
+            <Droppable key={column.id} droppableId={column.id}>
+              {(provided) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={{
+                    width: 240,
+                    backgroundColor: 'lightgrey',
+                    padding: 8,
+                    borderRadius: 4
+                  }}
+                >
+                  <h3>{column.title}</h3>
+                  {items.map((item, index) => (
+                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            userSelect: 'none',
+                            padding: 8,
+                            margin: '0 0 8px 0',
+                            borderRadius: 4,
+                            backgroundColor: 'grey',
+                            color: 'white',
+                            ...provided.draggableProps.style
+                          }}
+                        >
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          );
+        })}
       </div>
     </DragDropContext>
   );
