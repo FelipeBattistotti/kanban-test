d2efe446480d157f19b64ba1fe4aff8259ddedfb
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-09-29 00:03:42 -0300

    [UPDATE] 3

--- src/components/KanbanBoard/index.tsx
+++ src/components/KanbanBoard/index.tsx
@@ -1,145 +1,95 @@
-import React, { useState } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { DragDropContext } from "react-beautiful-dnd";
+import DraggableElement from "../DraggableElement";
 
-type Item = {
-  id: string;
-  content: string;
+const DragDropContextContainer = styled.div`
+  padding: 20px;
+  border: 4px solid indianred;
+  border-radius: 6px;
+`;
+
+const ListGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 8px;
+`;
+
+// fake data generator
+const getItems = (count, prefix) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => {
+    const randomId = Math.floor(Math.random() * 1000);
+    return {
+      id: `item-${randomId}`,
+      prefix,
+      content: `item ${randomId}`
+    };
+  });
+
+const removeFromList = (list, index) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(index, 1);
+  return [removed, result];
 };
 
-type Column = {
-  id: string;
-  title: string;
-  items: Item[];
+const addToList = (list, index, element) => {
+  const result = Array.from(list);
+  result.splice(index, 0, element);
+  return result;
 };
 
-const initialData = {
-  columns: {
-    'column-1': {
-      id: 'column-1',
-      title: 'To Do',
-      itemsIds: ['item-1', 'item-2']
-    },
-    'column-2': {
-      id: 'column-2',
-      title: 'Done',
-      itemsIds: []
+const lists = ["todo", "inProgress", "done"];
+
+const generateLists = () =>
+  lists.reduce(
+    (acc, listKey) => ({ ...acc, [listKey]: getItems(10, listKey) }),
+    {}
+  );
+
+function KanbanBoard() {
+  const [elements, setElements] = React.useState(generateLists());
+
+  useEffect(() => {
+    setElements(generateLists());
+  }, []);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
     }
-  },
-  items: {
-    'item-1': { id: 'item-1', content: 'Item 1' },
-    'item-2': { id: 'item-2', content: 'Item 2' }
-  },
-  columnOrder: ['column-1', 'column-2']
-};
+    const listCopy = { ...elements };
 
-const KanbanBoard: React.FC = () => {
-  const [data, setData] = useState(initialData);
+    const sourceList = listCopy[result.source.droppableId];
+    const [removedElement, newSourceList] = removeFromList(
+      sourceList,
+      result.source.index
+    );
+    listCopy[result.source.droppableId] = newSourceList;
+    const destinationList = listCopy[result.destination.droppableId];
+    listCopy[result.destination.droppableId] = addToList(
+      destinationList,
+      result.destination.index,
+      removedElement
+    );
 
-  const onDragEnd = (result: any) => {
-    const { source, destination } = result;
-
-    if (!destination) return;
-
-    if (source.droppableId === destination.droppableId && source.index === destination.index) return;
-
-    const startColumn = data.columns[source.droppableId];
-    const endColumn = data.columns[destination.droppableId];
-
-    if (startColumn === endColumn) {
-      const newItemsIds = Array.from(startColumn.itemsIds);
-      newItemsIds.splice(source.index, 1);
-      newItemsIds.splice(destination.index, 0, result.draggableId);
-
-      const newColumn = {
-        ...startColumn,
-        itemsIds: newItemsIds
-      };
-
-      setData(prevData => ({
-        ...prevData,
-        columns: {
-          ...prevData.columns,
-          [newColumn.id]: newColumn
-        }
-      }));
-    } else {
-      const startItemsIds = Array.from(startColumn.itemsIds);
-      startItemsIds.splice(source.index, 1);
-      const newStartColumn = {
-        ...startColumn,
-        itemsIds: startItemsIds
-      };
-
-      const endItemsIds = Array.from(endColumn.itemsIds);
-      endItemsIds.splice(destination.index, 0, result.draggableId);
-      const newEndColumn = {
-        ...endColumn,
-        itemsIds: endItemsIds
-      };
-
-      setData(prevData => ({
-        ...prevData,
-        columns: {
-          ...prevData.columns,
-          [newStartColumn.id]: newStartColumn,
-          [newEndColumn.id]: newEndColumn
-        }
-      }));
-    }
+    setElements(listCopy);
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div style={{ display: 'flex', gap: '16px' }}>
-        {data.columnOrder.map(columnId => {
-          const column = data.columns[columnId];
-          const items = column.itemsIds.map(itemId => data.items[itemId]);
-
-          return (
-            <Droppable key={column.id} droppableId={column.id}>
-              {(provided) => (
-                <div
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  style={{
-                    width: 240,
-                    backgroundColor: 'lightgrey',
-                    padding: 8,
-                    borderRadius: 4
-                  }}
-                >
-                  <h3>{column.title}</h3>
-                  {items.map((item, index) => (
-                    <Draggable key={item.id} draggableId={item.id} index={index}>
-                      {(provided) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={{
-                            userSelect: 'none',
-                            padding: 8,
-                            margin: '0 0 8px 0',
-                            borderRadius: 4,
-                            backgroundColor: 'grey',
-                            color: 'white',
-                            ...provided.draggableProps.style
-                          }}
-                        >
-                          {item.content}
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          );
-        })}
-      </div>
-    </DragDropContext>
+    <DragDropContextContainer>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <ListGrid>
+          {lists.map((listKey) => (
+            <DraggableElement
+              elements={elements[listKey]}
+              key={listKey}
+              prefix={listKey}
+            />
+          ))}
+        </ListGrid>
+      </DragDropContext>
+    </DragDropContextContainer>
   );
-};
+}
 
 export default KanbanBoard
